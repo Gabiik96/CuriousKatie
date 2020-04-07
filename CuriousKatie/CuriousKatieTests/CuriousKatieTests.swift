cc17901c5c221bd5c6fcc83c748a15ca570530b9
Gabriel Balta <g.balta96@gmail.com>
2020-04-07 22:37:18 +0100

    XCTest case added for not skipping person which is supposed to get matched

--- CuriousKatie/CuriousKatieTests/CuriousKatieTests.swift
+++ CuriousKatie/CuriousKatieTests/CuriousKatieTests.swift
@@ -21,17 +21,27 @@ class CuriousKatieTests: XCTestCase {
 
     /// When there is an odd number -> remaining person will not get paired up
     func testOddNumberOfParticipants() {
-        var people: [Person] =   [Person(name: "Jonathan", age: 48, interest: Interest.possibleInterests),
-                                  Person(name: "Julia", age: 37, interest: Interest.possibleInterests),
-                                  Person(name: "Peter", age: 40, interest: Interest.possibleInterests),
-                                  Person(name: "Reynold", age: 29, interest: Interest.possibleInterests),
-                                  Person(name: "Bianca", age: 36, interest: Interest.possibleInterests),]
+        let people: [Person] =   [Person(name: "Jonathan", age: 48, interest: Interest.possibleInterests()),
+                                  Person(name: "Julia", age: 37, interest: Interest.possibleInterests()),
+                                  Person(name: "Peter", age: 40, interest: Interest.possibleInterests()),
+                                  Person(name: "Reynold", age: 29, interest: Interest.possibleInterests()),
+                                  Person(name: "Bianca", age: 36, interest: Interest.possibleInterests()),]
         
         XCTAssert(!CuriousKatie.matchPeople(participants: people).isEmpty, "Odd number of participants should leave one person out.")
     }
     /// Nobody who is supposed to get matched get skipped
-    func test() {
-        
+    func testMatchMatchingParticipants() {
+        let people: [Person] =   [Person(name: "Jonathan", age: 48, interest: matchingInterest()),
+                                  Person(name: "Julia", age: 37, interest: matchingInterest()),
+                                  Person(name: "Peter", age: 40, interest: matchingInterest()),
+                                  Person(name: "Reynold", age: 29, interest: nonMatchingInterest()),
+                                  Person(name: "Bianca", age: 36, interest: matchingInterest()),
+                                  Person(name: "Jozeph", age: 52, interest: matchingInterest())]
+        let matches = CuriousKatie.matchPeople(participants: people)
+        for each in matches {
+            print(each)
+        }
+        XCTAssert(matches.count == 2, "5 people with matchingInterest, should have 2 matches in array.")
     }
 
     func testPerformanceExample() {
@@ -39,6 +49,23 @@ class CuriousKatieTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func matchingInterest() -> [Interest] {
+    return [
+        Interest(title: "swimming", skill: Skill.allCases.randomElement()!),
+        Interest(title: "running", skill: Skill.allCases.randomElement()!),
+        Interest(title: "programming", skill: Skill.allCases.randomElement()!),
+        Interest(title: "snowboarding", skill: Skill.allCases.randomElement()!),
+        Interest(title: "skydiving", skill: Skill.allCases.randomElement()!)
+        ]
+    }
+    
+    func nonMatchingInterest() -> [Interest] {
+    return [
+        Interest(title: "racing", skill: Skill.allCases.randomElement()!),
+        Interest(title: "cycling", skill: Skill.allCases.randomElement()!)
+        ]
     }
 
 }
